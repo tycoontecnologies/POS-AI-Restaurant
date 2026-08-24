e238b40647c5507c01dcf5c5e313cf9b1b7976e2
tycoontecnologies <tycoontecnologies@gmail.com>
2026-08-24 09:53:41 +0500

    Align POS palette with MPS visual system

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,94 +1,94 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // International restaurant SaaS palette
-  static const Color primary = Color(0xFF0F766E);
-  static const Color primaryLight = Color(0xFF2DD4BF);
-  static const Color primaryDark = Color(0xFF115E59);
-  static const Color primarySoft = Color(0xFFCCFBF1);
+  // MPS-inspired product palette
+  static const Color primary = Color(0xFF6D3CF5);
+  static const Color primaryLight = Color(0xFF8B5CF6);
+  static const Color primaryDark = Color(0xFF5425D6);
+  static const Color primarySoft = Color(0xFFF1ECFF);
 
-  static const Color secondary = Color(0xFF2563EB);
+  static const Color secondary = Color(0xFF3B82F6);
   static const Color secondaryLight = Color(0xFF60A5FA);
-  static const Color secondaryDark = Color(0xFF1D4ED8);
+  static const Color secondaryDark = Color(0xFF2563EB);
 
   static const Color accent = Color(0xFFF59E0B);
   static const Color accentLight = Color(0xFFFBBF24);
   static const Color accentDark = Color(0xFFD97706);
 
-  static const Color success = Color(0xFF16A34A);
-  static const Color successLight = Color(0xFF22C55E);
-  static const Color successDark = Color(0xFF15803D);
-  static const Color successSoft = Color(0xFFDCFCE7);
+  static const Color success = Color(0xFF12B981);
+  static const Color successLight = Color(0xFF34D399);
+  static const Color successDark = Color(0xFF059669);
+  static const Color successSoft = Color(0xFFE6FAF3);
 
   static const Color warning = Color(0xFFF59E0B);
   static const Color warningLight = Color(0xFFFBBF24);
   static const Color warningDark = Color(0xFFD97706);
-  static const Color warningSoft = Color(0xFFFEF3C7);
+  static const Color warningSoft = Color(0xFFFFF4DE);
 
-  static const Color error = Color(0xFFDC2626);
-  static const Color errorLight = Color(0xFFEF4444);
-  static const Color errorDark = Color(0xFFB91C1C);
-  static const Color errorSoft = Color(0xFFFEE2E2);
+  static const Color error = Color(0xFFEF4444);
+  static const Color errorLight = Color(0xFFF87171);
+  static const Color errorDark = Color(0xFFDC2626);
+  static const Color errorSoft = Color(0xFFFFECEC);
 
-  static const Color info = Color(0xFF2563EB);
-  static const Color infoLight = Color(0xFF3B82F6);
-  static const Color infoDark = Color(0xFF1D4ED8);
+  static const Color info = Color(0xFF3B82F6);
+  static const Color infoLight = Color(0xFF60A5FA);
+  static const Color infoDark = Color(0xFF2563EB);
 
   static const Color onPrimary = Colors.white;
   static const Color onSecondary = Colors.white;
-  static const Color onAccent = Color(0xFF111827);
+  static const Color onAccent = Color(0xFF0B1F44);
   static const Color onSuccess = Colors.white;
-  static const Color onWarning = Color(0xFF111827);
+  static const Color onWarning = Color(0xFF0B1F44);
   static const Color onError = Colors.white;
   static const Color onInfo = Colors.white;
 
   static const Color white = Colors.white;
-  static const Color black = Color(0xFF111827);
-  static const Color grey50 = Color(0xFFF9FAFB);
-  static const Color grey100 = Color(0xFFF3F4F6);
-  static const Color grey200 = Color(0xFFE5E7EB);
-  static const Color grey300 = Color(0xFFD1D5DB);
-  static const Color grey400 = Color(0xFF9CA3AF);
-  static const Color grey500 = Color(0xFF6B7280);
-  static const Color grey600 = Color(0xFF4B5563);
-  static const Color grey700 = Color(0xFF374151);
-  static const Color grey800 = Color(0xFF1F2937);
-  static const Color grey900 = Color(0xFF111827);
-  static const Color grey950 = Color(0xFF030712);
+  static const Color black = Color(0xFF0B1F44);
+  static const Color grey50 = Color(0xFFF8FAFF);
+  static const Color grey100 = Color(0xFFF2F5FB);
+  static const Color grey200 = Color(0xFFE7ECF5);
+  static const Color grey300 = Color(0xFFD8DFEB);
+  static const Color grey400 = Color(0xFFA5B0C2);
+  static const Color grey500 = Color(0xFF728096);
+  static const Color grey600 = Color(0xFF536176);
+  static const Color grey700 = Color(0xFF344258);
+  static const Color grey800 = Color(0xFF1C2C46);
+  static const Color grey900 = Color(0xFF0B1F44);
+  static const Color grey950 = Color(0xFF06142E);
 
-  static const Color backgroundLight = Color(0xFFF6F7F9);
-  static const Color backgroundDark = Color(0xFF0B1120);
+  static const Color backgroundLight = Color(0xFFF4F7FF);
+  static const Color backgroundDark = Color(0xFF08162D);
   static const Color surfaceLight = Colors.white;
-  static const Color surfaceDark = Color(0xFF111827);
-  static const Color surfaceVariantLight = Color(0xFFF3F4F6);
-  static const Color surfaceVariantDark = Color(0xFF1F2937);
+  static const Color surfaceDark = Color(0xFF111E36);
+  static const Color surfaceVariantLight = Color(0xFFF6F8FC);
+  static const Color surfaceVariantDark = Color(0xFF1C2C46);
 
-  static const Color disabledLight = Color(0xFFE5E7EB);
-  static const Color disabledDark = Color(0xFF374151);
-  static const Color onDisabledLight = Color(0xFF9CA3AF);
-  static const Color onDisabledDark = Color(0xFF6B7280);
-  static const Color outlineLight = Color(0xFFE5E7EB);
-  static const Color outlineDark = Color(0xFF374151);
-  static const Color outlineVariantLight = Color(0xFFF3F4F6);
-  static const Color outlineVariantDark = Color(0xFF4B5563);
+  static const Color disabledLight = Color(0xFFE7ECF5);
+  static const Color disabledDark = Color(0xFF344258);
+  static const Color onDisabledLight = Color(0xFFA5B0C2);
+  static const Color onDisabledDark = Color(0xFF728096);
+  static const Color outlineLight = Color(0xFFE3E8F1);
+  static const Color outlineDark = Color(0xFF344258);
+  static const Color outlineVariantLight = Color(0xFFF0F3F8);
+  static const Color outlineVariantDark = Color(0xFF536176);
 
-  // Shell
-  static const Color sidebar = Color(0xFF111827);
-  static const Color sidebarCard = Color(0xFF1F2937);
-  static const Color sidebarBorder = Color(0xFF263244);
-  static const Color sidebarText = Color(0xFFD1D5DB);
-  static const Color sidebarMuted = Color(0xFF8B98AA);
+  // Compact shell
+  static const Color sidebar = Color(0xFF0C1B33);
+  static const Color sidebarCard = Color(0xFF152640);
+  static const Color sidebarBorder = Color(0xFF20324E);
+  static const Color sidebarText = Color(0xFFD9E2F0);
+  static const Color sidebarMuted = Color(0xFF91A0B5);
 
   // Restaurant states
-  static const Color tableAvailable = Color(0xFF16A34A);
-  static const Color tableOccupied = Color(0xFFDC2626);
-  static const Color tableReserved = Color(0xFF2563EB);
+  static const Color tableAvailable = Color(0xFF12B981);
+  static const Color tableOccupied = Color(0xFFEF4444);
+  static const Color tableReserved = Color(0xFF3B82F6);
   static const Color tableBilling = Color(0xFFF59E0B);
-  static const Color dineIn = Color(0xFF0F766E);
-  static const Color takeAway = Color(0xFF2563EB);
-  static const Color delivery = Color(0xFFF59E0B);
+  static const Color dineIn = Color(0xFF6D3CF5);
+  static const Color takeAway = Color(0xFF3B82F6);
+  static const Color delivery = Color(0xFF12B981);
   static const Color kotPending = Color(0xFFF59E0B);
-  static const Color kotPreparing = Color(0xFF2563EB);
-  static const Color kotReady = Color(0xFF16A34A);
-  static const Color kotServed = Color(0xFF6B7280);
+  static const Color kotPreparing = Color(0xFF3B82F6);
+  static const Color kotReady = Color(0xFF12B981);
+  static const Color kotServed = Color(0xFF728096);
 }
